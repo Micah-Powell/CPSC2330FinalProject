6296105f40d39a3681f979466a0933f4542cc863
Micah-Powell <MicahPowell2024@gmail.com>
2025-11-21 10:27:30 -0500

    Start of index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const submitbutton = document.querySelector('[type="button"]')
+
+console.log(submitbutton)
+
+submitbutton.addEventListener("click", addbutton)
+
+
+function addbutton() {
+    const name = document.querySelector('[type="text"]').value
+    const email = document.querySelector('[type="email"]').value
+
+    
+}
