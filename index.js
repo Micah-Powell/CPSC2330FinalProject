f40419722cf266f571872a40c00f54f104a3e63f
Micah-Powell <MicahPowell2024@gmail.com>
2025-11-21 10:51:16 -0500

    added sanitation check and list building

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ submitbutton.addEventListener("click", addbutton)
 function addbutton() {
     const name = document.querySelector('[type="text"]').value
     const email = document.querySelector('[type="email"]').value
+    if (email === "" || name === ""){
+        return
+    }
+    document.querySelector('[type="text"]').value = ""
+    document.querySelector('[type="email"]').value = ""
 
+    document.querySelector("tbody").innerHTML += (`<tr><td>${name}</td><td>${email}</td><td><button class="Delete">X</button></td></tr>`)
     
 }
