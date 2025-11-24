c68460e3962dc450b69ad723bd5c575a35ac7845
Micah-Powell <MicahPowell2024@gmail.com>
2025-11-24 09:38:16 -0500

    forgot to commit over the weekend

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 
 
 const submitbutton = document.querySelector('[type="button"]')
+const tablebody = document.querySelector('tbody')
 
 console.log(submitbutton)
+console.log(tablebody)
 
 submitbutton.addEventListener("click", addbutton)
+tablebody.addEventListener("click", deletebutton)
 
+
+
+function deletebutton(e) {
+    const target = e.target
+
+    const deletetarget = target.closest('TR')
+    console.log (target)
+    console.log (target.innerHTML)
+    console.log (deletetarget)
+    console.log (target.value == 'X')
+    if (target.innerHTML === '<button class="Delete">X</button>') {
+        document.querySelector('tbody').innerHTML.replace(deletetarget, "")
+    }
+}
 
 function addbutton() {
     const name = document.querySelector('[type="text"]').value
@@ -16,6 +33,6 @@ function addbutton() {
     document.querySelector('[type="text"]').value = ""
     document.querySelector('[type="email"]').value = ""
 
-    document.querySelector("tbody").innerHTML += (`<tr><td>${name}</td><td>${email}</td><td><button class="Delete">X</button></td></tr>`)
+    document.querySelector('tbody').innerHTML += (`<tr><td>${name}</td><td>${email}</td><td><button class="Delete">X</button></td></tr>`)
     
 }
